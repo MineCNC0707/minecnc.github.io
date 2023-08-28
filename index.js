f2172e67484e260a39ff47f76a3b65de117e3305
MineCNC <cncchow7777@gmail.com>
2023-08-29 00:26:17 +0800

    Co-authored-by: mikicomeback <mikicomeback@users.noreply.github.com>

--- index.js
+++
@@ -1,9 +0,0 @@
-// main.js
-document.addEventListener('DOMContentLoaded', function () {
-  const menuButton = document.getElementById('menuButton');
-  const drawer = document.getElementById('drawer');
-
-  menuButton.addEventListener('click', function () {
-    drawer.classList.toggle('hidden');
-  });
-});
